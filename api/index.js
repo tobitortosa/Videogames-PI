d9baf31506fb5ff490c3ccce48a3bdc4599924b9
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-19 12:33:39 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT || 3001, () => {
+  server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
   });
 });
