dcff5090a0d202a3f2f62dc712740f6503bfb179
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-19 11:57:12 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,13 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://mvtqokrt:r6nrVPnrvDFhSh_S7UhmG_TdRdts6AOZ@babar.db.elephantsql.com/mvtqokrt`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
 const basename = path.basename(__filename);
