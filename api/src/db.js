24f6447d3ec536a74fe7c98ae5cc51a42dc089ee
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-31 09:35:12 -0300

    finish

--- api/src/db.js
+++ api/src/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-// const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, POSTGRES_URL } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://mvtqokrt:r6nrVPnrvDFhSh_S7UhmG_TdRdts6AOZ@babar.db.elephantsql.com/mvtqokrt`,
+  POSTGRES_URL,
   {
     logging: false, // set to console.log to see the raw SQL queries
   }
